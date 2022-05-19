9dae8bdd6a0b073d995342804ca3558b6be37d1b
Kefan <40338496+KefanXu@users.noreply.github.com>
2022-05-19 15:25:28 -0400

    Delete secret.js

--- secret.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBCtI-JWQG8Fa3FvP98QutiJe0nNDsEZ6Q",
-  authDomain: "taperology.firebaseapp.com",
-  projectId: "taperology",
-  storageBucket: "taperology.appspot.com",
-  messagingSenderId: "734078016442",
-  appId: "1:734078016442:web:d99d3e928bf46273791b83",
-  measurementId: "G-BMJPMJGKH4",
-};
